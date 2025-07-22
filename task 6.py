994746c9811d80f4140e6d10561d6ae85514a03a
pop2399bytes <prakhar.parashar06@gmail.com>
2025-07-22 11:29:40 +0530

    Add files via upload

---
+++ task 6.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a number: "))
+from math import *
+if a>0:
+    print("Square root of", a, "is:",sqrt(a))    
+    print("Log of", a, "is:", log(a))
+    print("Sine of", a, "is:", sin(a))   
+else:
+    print("Invalid input")
+     
